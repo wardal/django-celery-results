903b05b2a250530b546206ac968b43d66c681f4a
wardal <igor.dubovik@outlook.com>
2017-10-26 19:40:06 +0300

    Add task name to admin filter

--- django_celery_results/admin.py
+++ django_celery_results/admin.py
@@ -11,6 +11,7 @@ class TaskResultAdmin(admin.ModelAdmin):
 
     model = TaskResult
     list_display = ('task_id', 'date_done', 'status')
+    list_filter = ('task_name', 'status', 'date_done')
     readonly_fields = ('date_done', 'result', 'hidden', 'meta')
     fieldsets = (
         (None, {
